fc67fac0e5482be827e51524022fce620db4cf05
dvaughn <dvaughn@mit.edu>
2013-01-16 15:37:02 -0500

    added skeleton migrations and changed gemfile to be heroku compatible

--- db/migrate/20130116004432_create_tables.rb
+++
@@ -1,7 +0,0 @@
-class CreateTables < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
---
+++ db/migrate/20130116203344_create_debates.rb
@@ -0,0 +1,7 @@
+class CreateDebates < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
