0f76dc2f5914913c73a772c3da2d2ae0d8ebd700
Derek Vaughn <dvaughn@mit.edu>
2013-01-15 16:33:41 -0800

    created empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassDebate::Application.config.secret_token = 'd4f470eeb9b2f6006ad84b1c94423fd44375ae8ecee964c8a5134fa55ceea0516ed94001143c759010ba4203ce3eb5ab6ce8275b041e338bc2018335329e0634'
